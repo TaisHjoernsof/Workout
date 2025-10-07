08a15326dd0a2102fa7326a14f2248b4428660cc
Tais <s193900@dtu.dk>
2025-10-07 17:51:38 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -823,7 +823,7 @@ input, select, textarea {
 /* Streak Counter */
 .streak-counter {
   position: fixed;
-  top: 20px;
+  top: 40px;
   right: 20px;
   background: rgba(255, 255, 255, 0.2);
   backdrop-filter: blur(10px);
