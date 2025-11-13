a7459eaad779e6ea6883307e2035208fafc9c0a3
Tais <s193900@dtu.dk>
2025-11-13 21:07:35 +0100

    Moved Streak Counter Position.

--- src/App.vue
+++ src/App.vue
@@ -999,7 +999,7 @@ input, select, textarea {
 /* Streak Counter */
 .streak-counter {
   position: fixed;
-  top: 20px;
+  top: 68px;
   right: 20px;
   background: rgba(255, 255, 255, 0.2);
   backdrop-filter: blur(10px);
